34ab697cb06e6b4e9e10b33e72960dfdf686aa4b
张世阳 <ZHANGSHIYANG162@pingan.com.cn>
2019-05-14 19:16:28 +0800

    master:[update]

--- src/ls/rm.ts
+++ src/ls/rm.ts
@@ -21,3 +21,23 @@ export async function rm(path) {
     }
     await rmdir(path);
 }
+export async function clear(path) {
+    if (!(await exists(path))) {
+        return;
+    }
+    const info = await lstat(path);
+    if (info.isFile()) {
+        await unlink(path);
+        return;
+    }
+    const files = await readdir(path);
+    for (const file of files) {
+        const curPath = path + '/' + file;
+        const cur_lstat = await lstat(curPath);
+        if (cur_lstat.isDirectory()) {
+            await rm(curPath);
+        } else {
+            await unlink(curPath);
+        }
+    }
+}
