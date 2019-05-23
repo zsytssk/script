1a70b42a2806c537fd47e9bbfb4afc09bcb18703
张世阳 <ZHANGSHIYANG162@pingan.com.cn>
2019-05-23 14:41:33 +0800

    master:[update]

--- test/test.ts
+++ test/test.ts
@@ -1,6 +1,12 @@
 import { calcClosestDepth, getFileInfo } from '../src/ls/pathUtil';
 
-calcClosestDepth('D:\\zsytssk\\github\\script\\', 'D:\\');
+calcClosestDepth('.vscode/launch.json', '.vscode');
+console.log(
+    calcClosestDepth(
+        'D:\\zsytssk\\github\\script\\src\\ls\\saddasasd',
+        'D:\\zsytssk\\github\\script\\src\\ls\\',
+    ),
+);
 
 getFileInfo('D:\\zsytssk\\github\\script\\src\\ls\\asyncUtil.ts');
 getFileInfo('D:\\zsytssk\\github\\script\\src\\ls\\');
