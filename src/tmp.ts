573679870df42f00210ddbc1f7efba69709cd366
zsytssk <zsytssk@gmail.com>
2021-12-29 17:10:18 +0800

    master:

--- src/tmp.ts
+++ src/tmp.ts
@@ -8,7 +8,7 @@ const zipPath = 'test.zip';
 //创建一最终打包文件的输出流
 const output = fs.createWriteStream(zipPath);
 //生成archiver对象，打包类型为zip
-const zipArchiver = archiver('zip');
+const zipArchiver = archiver('zip', { forceLocalTime: false });
 //将打包对象与输出流关联
 zipArchiver.pipe(output);
 for (let i = 0; i < files.length; i++) {
