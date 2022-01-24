138dd270ba89ef10ede64cc53e14abbfa5a3fa90
zsytssk <zsytssk@gmail.com>
2022-01-24 18:23:45 +0800

    master:

--- src/tmp.ts
+++ src/tmp.ts
@@ -1,20 +1,18 @@
-import * as archiver from 'archiver';
 import * as fs from 'fs';
 
-//被打包文件
-const files = ['bin/0.png', 'bin/1.png'];
+let cur = 1;
+function test() {
+    const prev = cur;
 
-const zipPath = 'test.zip';
-//创建一最终打包文件的输出流
-const output = fs.createWriteStream(zipPath);
-//生成archiver对象，打包类型为zip
-const zipArchiver = archiver('zip', { forceLocalTime: false });
-//将打包对象与输出流关联
-zipArchiver.pipe(output);
-for (let i = 0; i < files.length; i++) {
-    console.log(files[i]);
-    //将被打包文件的流添加进archiver对象中
-    zipArchiver.append(fs.createReadStream(files[i]), { name: files[i] });
+    return () => {
+        return prev === cur;
+    };
 }
-//打包
-zipArchiver.finalize();
+
+async function main() {
+    const fn = test();
+    cur = 2;
+    console.log(fn());
+}
+
+main();
